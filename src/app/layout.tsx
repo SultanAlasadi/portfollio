0a7d1ba960ac0a3f215365b98621fc5ce5cab6e6
Sultan Alasadi <159791988+SultanAlasadi@users.noreply.github.com>
2024-11-08 21:20:27 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -69,14 +68,7 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
 
-         <Script
-          id="wisepops-script"
-          strategy="lazyOnload"
-          data-cfasync="false"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,i,s,e){window[w]=window[w]||function(){(window[w].q=window[w].q||[]).push(arguments)};window[w].l=Date.now();s=document.createElement('script');e=document.getElementsByTagName('script')[0];s.defer=1;s.src=i;e.parentNode.insertBefore(s, e)})('wisepops', 'https://wisepops.net/loader.js?v=2&h=6t6PLN7X7h');`,
-          }}
-        />
+         
       </body>
     </html>
   );
